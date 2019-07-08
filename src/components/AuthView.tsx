5ad13644f813612817dc36256f0cf27d783896b3
FaizChishtie <faizchishtie@gmail.com>
2019-07-08 16:10:09 -0700

    initial commit

--- src/components/AuthView.tsx
+++ src/components/AuthView.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Text } from '@stardust-ui/react';
+import * as microsoftTeams from '@microsoft/teams-js';
 
 interface IAuthViewProps {
   title: string;
   url: string;
+  //handleAuthenticated: () => void;
 }
 
 export const AuthView: React.FC<IAuthViewProps> = (props: IAuthViewProps): JSX.Element => {
+
+  const onAuthSuccess = (result?: string | undefined): void => {
+    alert(`Successfully authenticated.\n${result}`);
+    //props.handleAuthenticated();
+  };
+
+  const onAuthFailure = (result?: string | undefined): void => {
+    alert(`Failed to authenticate.\n${result}`);
+  };
+
+  React.useEffect((): void => {
+    microsoftTeams.initialize();
+    microsoftTeams.appInitialization.notifyAppLoaded();
+    const authenticationParams: microsoftTeams.authentication.AuthenticateParameters = {
+      url: props.url,
+      successCallback: onAuthSuccess,
+      failureCallback: onAuthFailure,
+    }
+    microsoftTeams.authentication.registerAuthenticationHandlers(authenticationParams);
+  });
+
   return (
     <>
       <Text size={'large'} content={props.title} />
